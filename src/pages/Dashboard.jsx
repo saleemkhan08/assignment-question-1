2c532247206c487d712f7b2f80c3f18be9145090
Andy <ashishdeb1231@gmail.com>
2023-07-30 12:46:42 +0530

    Resolved total orders shown in header

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -15,42 +15,42 @@ import styles from "./Dashboard.module.css";
 import Card from "../component/card/Card";
 
 const Dashboard = () => {
-  const [currency, setCurrency] = useState("EUR");
-  const [searchText, setSearchText] = useState("");
-  const [selectedOrderDetails, setSelectedOrderDetails] = useState({});
-  const [selectedOrderTimeStamps, setSelectedOrderTimeStamps] = useState({});
+	const [currency, setCurrency] = useState("EUR");
+	const [searchText, setSearchText] = useState("");
+	const [selectedOrderDetails, setSelectedOrderDetails] = useState({});
+	const [selectedOrderTimeStamps, setSelectedOrderTimeStamps] = useState({});
 
-  return (
-    <div>
-      <div className={styles.header}>
-        <HeaderTitle primaryTitle="Orders" secondaryTitle="5 orders" />
-        <div className={styles.actionBox}>
-          <Search
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-          />
-          <Dropdown
-            options={["GBP", "USD", "JPY", "EUR"]}
-            onChange={(e) => setCurrency(e.target.value)}
-            selectedItem={currency}
-          />
-        </div>
-      </div>
-      <div className={styles.content}>
-        <div className={styles.section}>
-          <Card
-            cardData={selectedOrderDetails}
-            title="Selected Order Details"
-          />
-          <Card
-            cardData={selectedOrderTimeStamps}
-            title="Selected Order Timestamps"
-          />
-        </div>
-        <List rows={mockData.results} />
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className={styles.header}>
+				<HeaderTitle primaryTitle="Orders" secondaryTitle={`${mockData.results.length} orders`} />
+				<div className={styles.actionBox}>
+					<Search
+						value={searchText}
+						onChange={(e) => setSearchText(e.target.value)}
+					/>
+					<Dropdown
+						options={["GBP", "USD", "JPY", "EUR"]}
+						onChange={(e) => setCurrency(e.target.value)}
+						selectedItem={currency}
+					/>
+				</div>
+			</div>
+			<div className={styles.content}>
+				<div className={styles.section}>
+					<Card
+						cardData={selectedOrderDetails}
+						title="Selected Order Details"
+					/>
+					<Card
+						cardData={selectedOrderTimeStamps}
+						title="Selected Order Timestamps"
+					/>
+				</div>
+				<List rows={mockData.results} />
+			</div>
+		</div>
+	);
 };
 
 export default Dashboard;
