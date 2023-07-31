db043940acba7c7d38e5a68d485b858ed1e26d63
kunwarvk01 <kunwarvk01@gmail.com>
2023-07-31 21:47:52 +0530

    updated list

--- src/component/list/List.jsx
+++ src/component/list/List.jsx
@@ -4,16 +4,12 @@ import ListHeader from "./ListHeader";
 import ListHeaderCell from "./ListHeaderCell";
 import styles from "./List.module.css";
 
-const List = ({ rows, cur, search }) => {
+const List = ({ rows, cur, search, onSelect }) => {
   const filteredRows = search
     ? rows.filter((row) =>
         row["&id"].toLowerCase().includes(search.toLowerCase())
       )
     : rows;
-
-  const handleRowClick = (rowData) => {
-    console.log(rowData);
-  };
 
   return (
     <table className={styles.container}>
@@ -22,15 +18,17 @@ const List = ({ rows, cur, search }) => {
           <ListHeaderCell>Order ID</ListHeaderCell>
           <ListHeaderCell>Buy/Sell</ListHeaderCell>
           <ListHeaderCell>Country</ListHeaderCell>
+          <ListHeaderCell>Order Status</ListHeaderCell>
           <ListHeaderCell>Order Submitted</ListHeaderCell>
           <ListHeaderCell>Order Volume / {cur}</ListHeaderCell>
         </ListHeader>
       </thead>
       <tbody>
         {filteredRows.map((row) => (
-          <ListRow key={row["&id"]} onClick={() => handleRowClick(row)}>
+          <ListRow key={row["&id"]} onSelect={onSelect} row={row}>
             <ListRowCell>{row["&id"]}</ListRowCell>
             <ListRowCell>{row.executionDetails.buySellIndicator}</ListRowCell>
+            <ListRowCell>{row["&id"].split(":")[2]}</ListRowCell>
             <ListRowCell>{row.executionDetails.orderStatus}</ListRowCell>
             <ListRowCell>{row.timestamps.orderSubmitted}</ListRowCell>
             <ListRowCell>{row.bestExecutionData.orderVolume[cur]}</ListRowCell>
