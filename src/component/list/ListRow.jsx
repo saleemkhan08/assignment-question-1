74fd1bd8f64bc9cd4f875ca84d7efa4e7729322b
kunwarvk01 <kunwarvk01@gmail.com>
2023-07-31 21:48:03 +0530

    updated lisrow

--- src/component/list/ListRow.jsx
+++ src/component/list/ListRow.jsx
@@ -1,7 +1,11 @@
 import styles from "./ListRow.module.css";
 
-const ListCell = ({ children }) => {
-  return <tr className={styles.cell}>{children}</tr>;
+const ListCell = ({ children, onSelect, row }) => {
+  return (
+    <tr onClick={() => onSelect(row)} className={styles.cell}>
+      {children}
+    </tr>
+  );
 };
 
 export default ListCell;
